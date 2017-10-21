dc02afca9025426b558f8d1457ae5060e4f07b34
surekarajaram <32954117+surekarajaram@users.noreply.github.com>
2017-10-21 10:15:06 +0530

    Add files via upload

---
+++ Basic string.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16ece028
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16ece028 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+  mystring='programmer'
+  print len(mystring)
+
+if __name__ == '__main__':
+    main()
