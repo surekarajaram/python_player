7930c8ed857d0e82850ace6976f81c28c3ee4bcf
surekarajaram <32954117+surekarajaram@users.noreply.github.com>
2017-10-21 11:25:02 +0530

    Add files via upload

---
+++ Conditional statements.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16ece028
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16ece028 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    age=20
+    if(age>=18):
+     print("eligible")
+    else:
+     print("not eligible")
+if __name__ == '__main__':
+    main()
