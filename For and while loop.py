3cec3187f916052c6516f409160740391af04a26
surekarajaram <32954117+surekarajaram@users.noreply.github.com>
2017-10-21 13:49:30 +0530

    Add files via upload

---
+++ For and while loop.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+# Name:        module3
+# Purpose:
+#
+# Author:      16ece028
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16ece028 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+
+
+ for letter in 'Python':
+   print 'Current Letter :', letter
+fruits = ['banana', 'apple',  'mango']
+for fruit in fruits:
+   print 'Current fruit :', fruit
+print "Good bye!"
+
+if __name__ == '__main__':
+    main()
